869abd0028265ef53843bc15c7760c39a1b41a25
BaeYoona <jinalks43@gmail.com>
2025-04-10 12:10:42 +0900

    HW03: Complete Door Animation

---
+++ Assets/HW03_Animation/HW03_Animator1_Door1_Controller.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HW03_Animator1_Door1_Controller : MonoBehaviour
+{
+    public Animator Animator;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Animator.SetInteger("Status", 1);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Animator.SetInteger("Status", 0);
+    }
+}
